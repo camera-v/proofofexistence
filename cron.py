3bc5ca19e125074ff062222651f7d14c1d59f353
harlo <harlo.holmes@gmail.com>
2015-03-26 17:07:35 -0400

    minor PII from original dev removed

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ import datetime
 from model import Document
 from google.appengine.api import mail
 
-from secrets import ADMIN_EMAIL
+from secrets import ADMIN_EMAIL, DEFAULT_SENDER_EMAIL
 from blockchain import auto_consolidate
 
 class ConsolidationCron(webapp2.RequestHandler):
@@ -27,10 +27,10 @@ class ConfirmationCron(webapp2.RequestHandler):
     actionable = Document.get_actionable()
     for d in actionable:
       ret = d.blockchain_certify()
-      sender_address = "manuelaraoz@gmail.com"
+
       subject = "Document certified: %s %s" % (ret['success'], d.digest)
       body = subject + "\n\nmesage: %s" % (ret['message'])
-      mail.send_mail(sender_address, ADMIN_EMAIL, subject, body)
+      mail.send_mail(DEFAULT_SENDER_EMAIL, ADMIN_EMAIL, subject, body)
 
 class PaymentCheckerCron(webapp2.RequestHandler):
   def get(self):
